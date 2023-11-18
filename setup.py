bd998d3d1fd324221a0a44ac4e3a99f4f32aadbc
Aman Raj <88227750+amanraj-bose@users.noreply.github.com>
2023-11-18 12:16:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,12 +33,14 @@ class Setup(object):
         x = os.path.abspath(os.path.dirname(__file__))
         for i in os.listdir(x):
             if zipfile.is_zipfile(i):
-                file = str(i).split("-")[0]
-                if file == "models":
-                    self.unzip(os.path.join(x, i))
-                    return True
+                if (i.split("-")[0]) == "models":
+                    self.unzip(os.path.abspath(os.path.join(x, i)))
+                    self.style("Models Zip file Unzipped", "Installation is successful", "green", "#")
+                    self.style("Run this Command to start the server", f"python{x} app.py")
+                else:
+                    print("-", end="")
             else:
-                return False
+                self.style("Models Zip file hasn't Found", "Try Again !!", "red", "-")
             
     def is_internet(self):
         try:
@@ -52,13 +54,12 @@ class Setup(object):
 
     def setup(self):
         system = self.system()
-        creator = self.creator()
+        self.creator()
         self.style("Name of the OS", system["name"])
         self.style("Type os OS", system["os"])
         self.style("Type of Platform", system["platform"])
         self.style("Version of Installed python", system["version"])
         self.style("Creating Directories", "")
-        print(creator)
         self.style("Directories has done", "", "green", "#")            
         self.style("Installing Python Packages", "")
         x = ""
@@ -70,12 +71,7 @@ class Setup(object):
             self.style("Python Packages has Installed", str(bool(status[0])), "green", "#")
         else:
             self.style("Python Packages hasnot Installed due to your Internet Connection", "", "red", "-")
-        analyze = self._analyze()
-        if analyze:
-            self.style("Models Zip file Unzipped", "Installation is successful", "green", "#")
-            self.style("Run this Command to start the server", f"python{x} app.py")
-        else:
-            self.style("Models Zip file hasn't Unzipped", "Try Again !!", "red", "-")
+        self._analyze()
 
 if __name__ == '__main__':
     setup = Setup()
